21c571e24fea21ee60d40a3c2db4fe5ba51bd0d7
Eric Dong <eric2043@gmail.com>
2024-11-14 21:55:59 -0500

    update dotfiles

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dotfiles
+
+Personal dotfile repo
+
+Things not configured yet:
+- .xinitrc
+- .zlogin
+- .zlogout
+- .ssh
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+current_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 sudo pacman -Syu ansible python-argcomplete
 sudo activate-global-python-argcomplete
 ansible-galaxy collection install community.general
+
+ansible-playbook -K ${current_dir}ansible/bootstrap.yml
 
