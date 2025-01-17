857bd3b68c33293b7ba2eccb8b486e1849986e46
Eric Dong <eric2043@gmail.com>
2025-01-17 14:23:55 -0500

    draft changes

--- scripts/restore-desktop.sh
+++ scripts/restore-desktop.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-# original values
-width=1920
-height=1080
-dpi=96
-nvidia-settings -a CurrentMetaMode="DPY-1: 1920x1080_144 @${width}x${height} +0+0 {ViewPortIn=${width}x${height}, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+SCRIPTS_HOME=$HOME/scripts
 
-# restore dpi
-echo "Xft.dpi: $dpi" | xrdb
-$HOME/scripts/wallpaper.sh
-pkill dwm
+${SCRIPTS_HOME}/set-nvidia-display.sh
 slstatus &
 
