d84ce39b7fa1046801ad15ff615a74562a31c07b
Eric Dong <eric2043@gmail.com>
2025-01-23 21:50:04 -0500

    update scripts

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -3,5 +3,5 @@ wallpaper_image="/home/eric/Pictures/backgrounds/6138367-art-stars-fantasy-magic
 #~/Pictures/backgrounds/digital_mountain_clouds.jpg
 ~/venv/tools/bin/python -m pywal -n -i ${wallpaper_image} --backend schemer2
 
-feh --no-xinerama --bg-scale ${wallpaper_image}
-# feh --bg-scale ${wallpaper_image}
+#feh --no-xinerama --bg-scale ${wallpaper_image}
+feh --bg-scale ${wallpaper_image}
