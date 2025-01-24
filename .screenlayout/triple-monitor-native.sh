95c46c48b8015f052908d04059e06ec4bbfcea57
Eric Dong <eric2043@gmail.com>
2025-01-23 21:50:04 -0500

    update scripts

---
+++ .screenlayout/triple-monitor-native.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+xrandr \
+  --output HDMI-0 --mode 1920x1080 --pos 1754x0 --rotate normal \
+  --output DP-0 --mode 1920x1080 --rate 144 --pos 2560x1080 --rotate normal \
+  --output DP-1 --off \
+  --output DP-2 --off \
+  --output DP-3 --off \
+  --output DP-4 --primary --mode 2560x1440 --pos 0x1080 --rotate normal \
+  --output DP-5 --off
+
+source $HOME/scripts/wallpaper.sh
+# feh --no-xinerama --bg-fill ${wallpaper_image}
+feh --bg-fill ${wallpaper_image}
+
