3a9cec323a759f02cf2d0d2cba241e2275f70320
Eric Dong <eric2043@gmail.com>
2025-03-01 22:00:37 -0500

    Adjusted git prefix to lowercase g; added keybinding to stage hunk

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,6 +5,7 @@ return {
 		require("gitsigns").setup({})
 
 		-- Set a vim motion to <Space> + g + h to preview changes to the file under the cursor in normal mode
-		vim.keymap.set("n", "<leader>Gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
+		vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
+		vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "[G]it Stage [H]unk" })
 	end,
 }
