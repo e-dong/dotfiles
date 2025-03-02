54750a036ca5f621767a82652bc6856b733c117f
Eric Dong <eric2043@gmail.com>
2025-03-01 22:51:21 -0500

    Updated keymaps to open diagnostics and tab splits

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -13,13 +13,18 @@ vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to clipboard" })
 -- Paste from clipboard with <leader>p
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
 
+-- line diagnostics
+vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "[L]ine [D]iagnostic" })
+
 -- Split Management
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split window vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "split window horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "close current split" })
+
 -- tab management
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>ts", "<cmd>tab split<CR>", { desc = "Split pane to new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
