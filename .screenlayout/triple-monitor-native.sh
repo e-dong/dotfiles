1981f079488911aeadd2459aad4fab819d51b756
Eric Dong <eric2043@gmail.com>
2025-03-21 21:37:01 -0400

    adjusted screenlayout for new setup

--- .screenlayout/triple-monitor-native.sh
+++ .screenlayout/triple-monitor-native.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 xrandr \
-  --output HDMI-0 --mode 1920x1080 --pos 1754x0 --rotate normal \
-  --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 2560x1080 --rotate normal \
+  --output HDMI-0 --mode 1920x1080 --pos 0x180 --rotate normal \
+  --output DP-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
   --output DP-1 --off \
-  --output DP-2 --off \
+  --output DP-2 --mode 1920x1080 --pos 4480x180 --rotate normal \
   --output DP-3 --off \
-  --output DP-4 --mode 2560x1440 --pos 0x1080 --rotate normal \
+  --output DP-4 --off \
   --output DP-5 --off
 
 source $HOME/scripts/wallpaper.sh
