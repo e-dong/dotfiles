357fba4000cf9ed0361cd3d9efac3fc310063637
Eric Dong <eric2043@gmail.com>
2025-05-16 22:11:34 -0400

    Updated display scripts with new monitor setup

--- scripts/set-nvidia-display.sh
+++ scripts/set-nvidia-display.sh
@@ -7,7 +7,7 @@ dpi=${3:-96}
 
 # Set custom resolution on the host via nvidia-settings
 nvidia-settings -a \
-  CurrentMetaMode="DPY-1: 1920x1080_144 @${width}x${height} +0+0 {ViewPortIn=${width}x${height}, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+  CurrentMetaMode="DPY-3: 1920x1080_144 @${width}x${height} +0+0 {ViewPortIn=${width}x${height}, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
 
 echo "Xft.dpi: $dpi" | xrdb
 $HOME/scripts/wallpaper.sh
--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wallpaper_image="/home/eric/Pictures/backgrounds/6138367-art-stars-fantasy-magic-clouds-orginal-sky-scenery.jpg"
-#~/Pictures/backgrounds/digital_mountain_clouds.jpg
+wallpaper_image=/home/eric/Pictures/backgrounds/digital_mountain_clouds.jpg
 ~/venv/tools/bin/python -m pywal -n -i ${wallpaper_image} --backend schemer2
 
 #feh --no-xinerama --bg-scale ${wallpaper_image}
