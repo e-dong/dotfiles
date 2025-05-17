a4b92bf668241b1b759cc8d725c208e172f4c331
Eric Dong <eric2043@gmail.com>
2025-05-16 21:57:31 -0400

    Switch to use method call instead of command syntax

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,10 +2,12 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		-- setup gitsigns with default properties
-		require("gitsigns").setup({})
+		local gitsigns = require("gitsigns")
+    gitsigns.setup({})
 
 		-- Set a vim motion to <Space> + g + h to preview changes to the file under the cursor in normal mode
-		vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
-		vim.keymap.set({"n", "v"}, "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "[G]it Stage [H]unk" })
+		vim.keymap.set("n", "<leader>gh",gitsigns.preview_hunk, { desc = "[G]it Preview [H]unk" })
+		vim.keymap.set({"n", "v"}, "<leader>gs", gitsigns.stage_hunk, { desc = "[G]it [S]tage Hunk" })
+		vim.keymap.set({"n", "v"}, "<leader>gr", gitsigns.reset_hunk, { desc = "[G]it [R]eset Hunk" })
 	end,
 }
