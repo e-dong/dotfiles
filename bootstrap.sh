fff41c944071c7957808c1c2f0a93e2236ad22bb
Eric Dong <eric2043@gmail.com>
2025-05-30 09:19:53 -0400

    Make sure bootstrap has the correct inventory passed

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ sudo pacman -Syu --noconfirm ansible python-argcomplete
 sudo activate-global-python-argcomplete
 ansible-galaxy collection install community.general
 
-ansible-playbook -K ${current_dir}/ansible/bootstrap.yml
+ansible-playbook -K ${current_dir}/ansible/bootstrap.yml -i ${current_dir}/ansible/inventory/os.yaml
 
