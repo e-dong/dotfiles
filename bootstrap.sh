e72b0d57a82f8b8ade2e3f07339ef0a0be5114c4
Eric Dong <eric2043@gmail.com>
2025-05-30 22:17:10 -0400

    Added aur builder

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 current_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 sudo pacman -Syu --noconfirm ansible python-argcomplete
 sudo activate-global-python-argcomplete
-ansible-galaxy collection install community.general
+ansible-galaxy collection install community.general kewlfft.aur
 
 ansible-playbook -K ${current_dir}/ansible/bootstrap.yml -i ${current_dir}/ansible/inventory/os.yaml
 
