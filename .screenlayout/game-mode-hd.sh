193ad9bb97a3f43005e2d9aa99d23bcdfb69a2de
Eric Dong <eric2043@gmail.com>
2026-02-11 20:59:13 -0500

    Updated layout scripts

---
+++ .screenlayout/game-mode-hd.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
+source $HOME/scripts/wallpaper.sh
+feh --bg-fill ${wallpaper_image}
